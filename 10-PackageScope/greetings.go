070a02e28cabd763601e3501152c2d43f89d66c6
Ida Danuartha <danuart21@gmail.com>
2024-03-09 00:26:47 +0800

    package scope

---
+++ 10-PackageScope/greetings.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var points = []int{20,50,80,100}
+
+func sayHello(n string) {
+	fmt.Println("Hello,", n)
+}
+
+func showScore() {
+	fmt.Println("you scored this many points:", score)
+}
---
+++ 10-PackageScope/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var score = 90
+
+func main() {
+    sayHello("Danuartha")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScore()
+}
