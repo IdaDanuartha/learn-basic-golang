81d417a6120117ea960498458b8313c8486076f3
Ida Danuartha <danuart21@gmail.com>
2024-03-09 12:02:32 +0800

    receiver functions

---
+++ 15-ReceiverFunctions/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		tip:   0,
+	}
+	return b
+}
+
+// format the bill
+func (b bill) format() string {
+	fs := "Bill breakdown:\n"
+	var total float64 = 0
+
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
+		total += v
+	}
+
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total)
+
+	return fs
+}
+
+func main() {
+	mybill := newBill("mario's bill")
+	mybill2 := newBill("danu's bill")
+
+	fmt.Println(mybill.format())
+	fmt.Println(mybill2.format())
+}
