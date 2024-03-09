6bfda7f53e628f4f91f3897da2193c4fffa225f0
Ida Danuartha <danuart21@gmail.com>
2024-03-09 12:13:31 +0800

    parsing floats

---
+++ 19-ParsingFloats/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type bill struct {
+	name  string
+	items map[string]float64
+	tip   float64
+}
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
+	}
+	return b
+}
+
+// add item to bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+// format the bill
+func (b *bill) format() string {
+	fs := "Bill breakdown:\n"
+	var total float64 = 0
+
+	// list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v\n", k+":", v)
+		total += v
+	}
+
+	// add tip
+	fs += fmt.Sprintf("%-25v ...$%v\n", "tip:", b.tip)
+
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total+b.tip)
+
+	return fs
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	(*b).tip = tip
+	// b.tip = tip
+}
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill -", b.name)
+
+	return b
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number...")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
+		fmt.Println("item added -", name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number...")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("tip has been updated to", tip)
+		promptOptions(b)
+	case "s":
+		fmt.Println("you chose to save the bill", b)
+	default:
+		fmt.Println("That was not a valid option...")
+		promptOptions(b)
+	}
+}
+
+func main() {
+
+	mybill := createBill()
+	promptOptions(mybill)
+
+}
